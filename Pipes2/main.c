6798baabe5664ac96dc2eb9ff1ffa63fbb2b8be5
Fredy Goolberth Quispe Neira <fredy.quispe@ucsp.edu.pe>
2023-04-10 21:52:25 -0500

    Actualization

--- Pipes2/main.c
+++ Pipes2/main.c
@@ -19,7 +19,7 @@ void sighandler() {
 int main(void)
 {
 	srand(time(0));
-	int rand_num, n_b;
+	int rand_num= 0, n_b;
 	char buff[SZ];
 	char str[SZ];	
 	const char *fn= "/tmp/myfifo";
@@ -28,37 +28,47 @@ int main(void)
 		if(s== 1){
 			rand_num= rand()%1000;
        			int     fd[2], fd_f;
-       			pid_t   childpid1= -1;
-			pid_t	childpid2= -1;
-        		pipe(fd);//fd[0]= read fd[1]= write
-			close(fd[1]);
-			n_b= sprintf(str, "%d", rand_num);
-			str[n_b]= 0;
-			write(fd[1], str, strlen(str)+1);
-			if (rand_num%2) childpid2= fork();
-			else childpid1= fork();
-        		if(childpid1 == 0){
+       			pid_t   id_odd, id_even;
+        		id_even= fork();
+			if(id_even == 0){
                 		//Even
-                		close(fd[0]);
-                		n_b= read(fd[0], buff, SZ);
-				buff[n_b]= 0;
-				mkfifo(fn, 0666);
-				fd_f= open(fn, O_WRONLY);
-				printf("%s", buff);
-				write(fd_f, buff, strlen(buff)+1);
-                		return 0;
-        		}
-        		else if (childpid2 == 0){
-				//Odd
-				close(fd[0]);
-				n_b= read(fd[0], buff, SZ);
-				buff[n_b]= 0;
-				mkfifo(fn, 0666);
+                		if(rand_num && !rand_num%2){
+					close(fd[0]);
+                			n_b= read(fd[0], buff, SZ);
+					buff[n_b]= 0;
+					printf("Even: %s\n", buff);
+					mkfifo(fn, 0666);
+					fd_f= open(fn, O_WRONLY);
+					write(fd_f, buff, strlen(buff)+1);
+                			close(fd_f);
+				}
 				return 0;
-			}
-			else if(childpid1+childpid2==-2){
-				printf("Error fork\n");
-				exit(1);
+        		}
+        		else{
+				id_odd= fork();
+			       	if (id_odd == 0){
+					//Odd
+					if(rand_num && rand_num%2){
+						close(fd[0]);
+						n_b= read(fd[0], buff, SZ);
+						buff[n_b]= 0;
+						printf("Odd: %s\n", buff);
+						mkfifo(fn, 0666);
+						fd_f= open(fn, O_WRONLY);
+						write(fd_f, buff, strlen(buff)+1);
+						close(fd_f);
+					}
+					return 0;
+				}
+				else{
+					//Parent
+					rand_num= rand()%1000;
+					pipe(fd);
+					close(fd[1]);
+					sprintf(str, "%d", rand_num);
+					printf("%s\n", str);
+					write(fd[1], str, strlen(str)+1);
+				}
 			}
 			s= 0;
 		}
